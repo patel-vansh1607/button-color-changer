c8d460f103b8c4efb2dbb52b8995e1be4af3d0d5
Vansh Patel <patel.vansh1607@gmail.com>
2025-04-02 13:33:07 +0300

    adding classnames to all

--- src/components/button.js
+++ src/components/button.js
@@ -6,26 +6,26 @@ const ButtonColorChanger = () => {
     return(
         <div className="maindiv" style={{backgroundColor: bgColor, height: "100vh"}}>
             <div className="secdiv">
-                <button onClick={() =>setBgColor ("black")}>Black Background</button>
-                <button onClick={() =>setBgColor ("white")}>White Background</button>
-                <button onClick={() =>setBgColor ("red")}>Red Background</button>
-                <button onClick={() =>setBgColor ("green")}>Green Background</button>
-                <button onClick={() =>setBgColor ("blue")}>Blue Background</button>
-                <button onClick={() =>setBgColor ("yellow")}>Yellow Background</button>
-                <button onClick={() =>setBgColor ("cyan")}>Cyan Background</button>
-                <button onClick={() =>setBgColor ("magenta")}>Magenta Background</button>
-                <button onClick={() =>setBgColor ("gray")}>Gray Background</button>
-                <button onClick={() =>setBgColor ("darkgray")}>Dark Gray Background</button>
-                <button onClick={() =>setBgColor ("lightgray")}>Light Gray Background</button>
-                <button onClick={() =>setBgColor ("brown")}>Brown Background</button>
-                <button onClick={() =>setBgColor ("orange")}>Orange Background</button>
-                <button onClick={() =>setBgColor ("purple")}>Purple Background</button>
-                <button onClick={() =>setBgColor ("pink")}>Pink Background</button>
-                <button onClick={() =>setBgColor ("lime")}>Lime Background</button>
-                <button onClick={() =>setBgColor ("navy")}>Navy Background</button>
-                <button onClick={() =>setBgColor ("teal")}>Teal Background</button>
-                <button onClick={() =>setBgColor ("gold")}>Gold Background</button>
-                <button onClick={() =>setBgColor ("silver")}>Silver Background</button>
+                <button className="button" onClick={() =>setBgColor ("black")}>Black Background</button>
+                <button className="button" onClick={() =>setBgColor ("white")}>White Background</button>
+                <button className="button" onClick={() =>setBgColor ("red")}>Red Background</button>
+                <button className="button" onClick={() =>setBgColor ("green")}>Green Background</button>
+                <button className="button" onClick={() =>setBgColor ("blue")}>Blue Background</button>
+                <button className="button" onClick={() =>setBgColor ("yellow")}>Yellow Background</button>
+                <button className="button" onClick={() =>setBgColor ("cyan")}>Cyan Background</button>
+                <button className="button" onClick={() =>setBgColor ("magenta")}>Magenta Background</button>
+                <button className="button" onClick={() =>setBgColor ("gray")}>Gray Background</button>
+                <button className="button" onClick={() =>setBgColor ("darkgray")}>Dark Gray Background</button>
+                <button className="button" onClick={() =>setBgColor ("lightgray")}>Light Gray Background</button>
+                <button className="button" onClick={() =>setBgColor ("brown")}>Brown Background</button>
+                <button className="button" onClick={() =>setBgColor ("orange")}>Orange Background</button>
+                <button className="button" onClick={() =>setBgColor ("purple")}>Purple Background</button>
+                <button className="button" onClick={() =>setBgColor ("pink")}>Pink Background</button>
+                <button className="button" onClick={() =>setBgColor ("lime")}>Lime Background</button>
+                <button className="button" onClick={() =>setBgColor ("navy")}>Navy Background</button>
+                <button className="button" onClick={() =>setBgColor ("teal")}>Teal Background</button>
+                <button className="button" onClick={() =>setBgColor ("gold")}>Gold Background</button>
+                <button className="button" onClick={() =>setBgColor ("silver")}>Silver Background</button>
 
             </div>
         </div>
